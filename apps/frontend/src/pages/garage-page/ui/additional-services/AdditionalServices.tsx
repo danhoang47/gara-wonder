4482c1d3a6283ce97f768a5084520d1da50d62f0
naga105 <huy1152002@gmail.com>
2024-01-29 20:01:41 +0700

    pages/gara-detail fix component SupportChip + fix merge

--- apps/frontend/src/pages/garage-page/ui/additional-services/AdditionalServices.tsx
+++ apps/frontend/src/pages/garage-page/ui/additional-services/AdditionalServices.tsx
@@ -1,4 +1,49 @@
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
+const fakeData = [
+    {
+        id: "1",
+        additionalServicesName: "Serving drinks while waiting ",
+        icon: faCoffee,
+    },
+    {
+        id: "1",
+        additionalServicesName: "Serving drinks while waiting ",
+        icon: faCoffee,
+    },
+    {
+        id: "1",
+        additionalServicesName: "Serving drinks while waiting ",
+        icon: faCoffee,
+    },
+    {
+        id: "1",
+        additionalServicesName: "Serving drinks while waiting ",
+        icon: faCoffee,
+    },
+];
 function AdditionalServices() {
-    return <></>;
+    const [additionalServices, setAdditionalServices] = useState(fakeData);
+    return (
+        <div>
+            <div className="pb-4">
+                <p className="text-2xl text-black font-medium">
+                    Additional Services
+                </p>
+            </div>
+            <div className="flex flex-col gap-4">
+                {additionalServices.map((service, index) => {
+                    return (
+                        <div className="flex items-center gap-4" key={index}>
+                            <FontAwesomeIcon icon={service.icon} />
+                            <p>{service.additionalServicesName}</p>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 export default AdditionalServices;
