d1e2b96e49e705cb0ad48a9d406e3b54cebca049
naga105 <huy1152002@gmail.com>
2024-02-17 14:34:46 +0700

    pages/gara-detail fix export index core

--- apps/frontend/src/core/ui/index.ts
+++ apps/frontend/src/core/ui/index.ts
@@ -1,7 +1,7 @@
 export { default as Header } from "./header";
 export { default as ProtectedRoute } from "./protected-route";
 export { default as Overlay } from "./overlay";
-export { default as BrandLogo } from './brand-logo'
+export { default as BrandLogo } from "./brand-logo";
 export { default as Input } from "./input";
 export { default as Select } from "./select";
 export { default as FileInput } from './file-input'
