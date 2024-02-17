2994ee3feb76461015e81ccdae897b7e7c697b63
quocDat040702 <dathoang.040702@gmail.com>
2024-02-17 11:01:30 +0700

    add Toasts

--- apps/frontend/src/features/user/index.ts
+++ apps/frontend/src/features/user/index.ts
@@ -1,2 +1,1 @@
 export * from './sign-in'
-export { default as userReducer } from './user.slice'
--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -2,14 +2,22 @@ import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
-  plugins: [nextui()]
-}
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    darkMode: "class",
+    plugins: [
+        nextui({
+            light: {
+                colors: {
+                    failure: "#f31260",
+                },
+            },
+        }),
+    ],
+};
