8c0955b40bc05e88a2a0bed5553da9a7c4998379
quocDat040702 <dathoang.040702@gmail.com>
2024-02-29 22:30:45 +0700

    add BookPage

--- apps/frontend/src/pages/book-page/BookPage.tsx
+++ apps/frontend/src/pages/book-page/BookPage.tsx
@@ -1,13 +1,14 @@
+import { Order } from "@/core/types";
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Button, Input } from "@nextui-org/react";
-import { useNavigate } from "react-router-dom";
-
+import { Button, Input, Select, SelectItem } from "@nextui-org/react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function BookPage() {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const { state }: { state: Order } = useLocation();
 
-    return (  
+    return (
         <div className="w-3/4 max-w-5xl md:w-full py-8 md:px-10 mx-auto">
             <div className="flex items-center gap-2 mb-8">
                 {/* <Button variant="light" isIconOnly radius="full" onPress={() => navigate(-1)}>
@@ -17,23 +18,38 @@ function BookPage() {
             </div>
             <div className="flex gap-4">
                 <div className="flex-grow">
-                    <p className="text-lg font-medium mb-4">Your booking information</p>
-                    <div className="flex flex-col gap-2">
-                        <Input 
-                            label="Date"
-                            placeholder="Date"
-                            value={new Date().toDateString()}
-                            variant="flat"
+                    <p className="text-lg font-medium mb-4">
+                        Your booking information
+                    </p>
+                    <div className="flex flex-col gap-4">
+                        <div className="flex justify-between items-center">
+                            <div>
+                                <p className="text-sm text-default-600">Date</p>
+                                <p className="">{new Date().toDateString()}</p>
+                            </div>
+                            <div>
+                                <p className="text-medium underline font-medium">
+                                    Edit
+                                </p>
+                            </div>
+                        </div>
+                        <Select
+                            variant="underlined"
+                            label="Services"
+                            placeholder="Services"
                             classNames={{
-                                inputWrapper: "bg-background"
+                                trigger: "!px-0 py-0",
+                                label: ""
                             }}
-                            isReadOnly
-                        />
+                            selectedKeys={["hello"]}
+                        >
+                            <SelectItem key="hello">
+                                <p>Category 1</p>
+                            </SelectItem>
+                        </Select>
                     </div>
                 </div>
-                <div className="flex-grow">
-
-                </div>
+                <div className="flex-grow"></div>
             </div>
         </div>
     );
