7cddf507b0e373937b1ce7a84a4efc2c2c1ce24c
Dan Hoang <85924986+danhoang47@users.noreply.github.com>
2024-03-11 10:04:19 +0000

    features/notifications

--- apps/frontend/src/features/notifications/Notifications.tsx
+++ apps/frontend/src/features/notifications/Notifications.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import NotificationsDialog from "./notifications-dialog";
 import {
+    Badge,
     Button,
     Popover,
     PopoverContent,
@@ -9,13 +10,16 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-regular-svg-icons";
 import { useAppSelector, useModalContext } from "@/core/hooks";
+import { hasAllNotificationsRead } from "./notifications.slice";
 
 function Notifications() {
     const { open } = useModalContext();
     const user = useAppSelector((state) => state.user.value);
+    const hasAllRead = useAppSelector(state => hasAllNotificationsRead(state.notifications))
     const [isNotificationsOpen, setNotificationsOpen] =
         useState<boolean>(false);
 
+    console.log(hasAllRead)
     return (
         <Popover
             isOpen={isNotificationsOpen && Boolean(user)}
@@ -37,7 +41,9 @@ function Notifications() {
                         }
                     }}
                 >
-                    <FontAwesomeIcon icon={faBell} size="lg" />
+                    <Badge isInvisible={hasAllRead} content="" color="primary" shape="circle" placement="bottom-right">
+                        <FontAwesomeIcon icon={faBell} size="lg" />
+                    </Badge>
                 </Button>
             </PopoverTrigger>
             <PopoverContent className="w-[420px] p-0">
--- apps/frontend/src/features/notifications/notifications-dialog/NotificationsDialog.tsx
+++ apps/frontend/src/features/notifications/notifications-dialog/NotificationsDialog.tsx
@@ -1,11 +1,68 @@
-import { useInfiniteScroll } from "@/core/hooks";
+import { useAppDispatch, useAppSelector, useAsyncList, useInfiniteScroll } from "@/core/hooks";
 import NotificationSkeleton from "../notification-skeleton";
+import { useCallback } from "react";
+import { Notification, Paging } from "@/core/types";
+import { getNotifications } from "@/api";
+import NotificationCard from "../notification-card";
+import { notificationUpsert, notificationsReceived, selectNotifications } from "../notifications.slice";
+
+const DEFAULT_PAGING: Paging = {
+    limit: 1
+}
 
 function NotificationsDialog() {
-    const ref = useInfiniteScroll(() => {
-        console.log("end");
-    });
+    const user = useAppSelector(state => state.user.value)
+    const notifications = useAppSelector(state => selectNotifications(state.notifications));
+    const dispatch = useAppDispatch()
+    const getKey = useCallback(() => {
+        if (!user) return null;
+        
+        return "notifications/" + user._id
+    }, [user?._id])
+    const onNotificationLoaded = (notifications: Notification[], isReload: boolean) => {
+        if (isReload) {
+            dispatch(notificationsReceived(notifications))
+        } else {
+            dispatch(notificationUpsert(notifications))
+        }
+    }
+    const { isReload, isLoading, onNext } = useAsyncList<Notification>(
+        getKey, 
+        onNotificationLoaded, 
+        [user], 
+        async (params: [string | null, Paging]) => {
+            const [_, paging] = params;
+            const results = await getNotifications(user?._id, paging.limit, paging?.nextCursor)
+            return results
+        },
+        DEFAULT_PAGING
+    )
+    const ref = useInfiniteScroll(onNext);
 
+    const onRenderLoading = () => {
+        return <>
+            {Array.from(new Array(10)).map((_, index) => (
+                <NotificationSkeleton key={index} />
+            ))}
+        </>
+    }
+
+    const onRenderNotifications = () => {
+        // TODO: need backend enhanced logic
+        // if (isReload) {
+        //     return <>{onRenderLoading()}</>
+        // } 
+        return (
+            <>
+                {notifications?.map(notification => (
+                    <NotificationCard key={notification._id} notification={notification}/>
+                ))}
+                {isLoading && onRenderLoading()}
+            </>
+        )
+    }
+
+    console.log(notifications)
     return (
         <div className="w-full flex flex-col">
             <div className="flex py-4 px-6 justify-between items-center border-b">
@@ -15,10 +72,8 @@ function NotificationsDialog() {
                 </p>
             </div>
             <div className="max-h-[70vh] overflow-y-auto">
-                {Array.from(new Array(20)).map((_, index) => (
-                    <NotificationSkeleton key={index} />
-                ))}
-                <div ref={ref} className="h-1"/>
+                {onRenderNotifications()}
+                <div className="h-1"/>
             </div>
         </div>
     );
