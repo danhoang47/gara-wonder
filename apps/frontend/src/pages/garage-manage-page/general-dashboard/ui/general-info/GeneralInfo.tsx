860d5f1e77697f871d76a2e4882e87894662de6b
naga105 <huy1152002@gmail.com>
2024-03-21 01:38:39 +0700

    pages/garage-management/general update UI

--- apps/frontend/src/pages/garage-manage-page/general-dashboard/ui/general-info/GeneralInfo.tsx
+++ apps/frontend/src/pages/garage-manage-page/general-dashboard/ui/general-info/GeneralInfo.tsx
@@ -1,59 +1,69 @@
-import { faEnvelopeOpen, faFileAlt } from "@fortawesome/free-regular-svg-icons";
-import { faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import { faCar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/react";
+import { Tab, Tabs } from "@nextui-org/react";
+import { useState } from "react";
 
 function GeneralInfo() {
+    const [selectType, setSelectType] = useState<string>("today");
+
     return (
-        <div className="flex flex-col md:flex-row justify-between w-full gap-5 pt-5 ">
-            <Card className="relative min-w-full md:min-w-[30rem] p-7">
-                <CardHeader className="z-0">
-                    <p className="font-bold text-2xl ">Staff onsite</p>
-                </CardHeader>
-                <CardBody>
-                    <p className="text-6xl font-semibold">
-                        26{" "}
-                        <span className="text-3xl text-default-500">/30</span>
-                    </p>
-                </CardBody>
-                <CardFooter>
-                    <FontAwesomeIcon
-                        icon={faUserGroup}
-                        size="5x"
-                        className="absolute right-8 bottom-8 text-default-600"
-                    />
-                </CardFooter>
-            </Card>
-            <Card className="relative min-w-full md:min-w-[30rem] p-7">
-                <CardHeader className="z-0">
-                    <p className="font-bold text-2xl">Slot available</p>
-                </CardHeader>
-                <CardBody>
-                    <p className="text-6xl font-semibold">4</p>
-                </CardBody>
-                <CardFooter>
-                    <FontAwesomeIcon
-                        icon={faFileAlt}
-                        size="5x"
-                        className="absolute right-8 bottom-8 text-default-600"
-                    />
-                </CardFooter>
-            </Card>
-            <Card className="relative min-w-full md:min-w-[30rem] p-7">
-                <CardHeader className="z-0">
-                    <p className="font-bold text-2xl">Unread Messages</p>
-                </CardHeader>
-                <CardBody>
-                    <p className="text-6xl font-semibold">26</p>
-                </CardBody>
-                <CardFooter>
-                    <FontAwesomeIcon
-                        icon={faEnvelopeOpen}
-                        size="5x"
-                        className="absolute right-8 bottom-8 text-default-600"
-                    />
-                </CardFooter>
-            </Card>
+        <div className="w-full border-b-1 pb-[7.5rem]">
+            <div className="flex justify-between items-center">
+                <p className="font-semibold text-3xl">
+                    Hôm nay bạn có gì mới ?
+                </p>
+                <Tabs
+                    color="primary"
+                    aria-label="Tabs colors"
+                    radius="sm"
+                    selectedKey={selectType}
+                    onSelectionChange={(e: React.Key) => {
+                        console.log(e);
+                        setSelectType(e as string);
+                    }}
+                >
+                    <Tab key="today" title="Hôm nay" />
+                    <Tab key="tommorow" title="Ngày mai" />
+                </Tabs>
+            </div>
+            <div className="flex justify-between pt-10">
+                <div className="min-w-32 h-32 p-5 flex justify-center items-center shadow-md rounded-lg border-1 border-default-100">
+                    <div>
+                        <div className="font-semibold text-2xl flex gap-3 items-center">
+                            <FontAwesomeIcon icon={faCar} />
+                            <p>0</p>
+                        </div>
+                        <p className="text-lg pt-3">Check-ins</p>
+                    </div>
+                </div>
+                <div className="min-w-32 h-32 p-5 flex justify-center items-center shadow-md rounded-lg border-1 border-default-100">
+                    <div>
+                        <div className="font-semibold text-2xl flex gap-3 items-center">
+                            <FontAwesomeIcon icon={faCar} />
+                            <p>0</p>
+                        </div>
+                        <p className="text-lg pt-3">Checkouts</p>
+                    </div>
+                </div>
+                <div className="min-w-32 h-32 p-5 flex justify-center items-center shadow-md rounded-lg border-1 border-default-100">
+                    <div>
+                        <div className="font-semibold text-2xl flex gap-3 items-center">
+                            <FontAwesomeIcon icon={faCar} />
+                            <p>0</p>
+                        </div>
+                        <p className="text-lg pt-3">Đang sửa chữa</p>
+                    </div>
+                </div>
+                <div className="min-w-32 h-32 p-5 flex justify-center items-center shadow-md rounded-lg border-1 border-default-100">
+                    <div>
+                        <div className="font-semibold text-2xl flex gap-3 items-center">
+                            <FontAwesomeIcon icon={faCar} />
+                            <p>0</p>
+                        </div>
+                        <p className="text-lg pt-3">Chờ đánh giá</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- apps/frontend/src/pages/garage-manage-page/general-dashboard/ui/update-garage/UpdateGarage.tsx
+++ apps/frontend/src/pages/garage-manage-page/general-dashboard/ui/update-garage/UpdateGarage.tsx
@@ -1,44 +1,64 @@
-import { change_info } from "../../img";
+import { faCar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Tab, Tabs } from "@nextui-org/react";
+import { useState } from "react";
 
 function UpdateGarage() {
+    const [selectType, setSelectType] = useState<string>("today");
     return (
-        <div className="pt-10">
-            <p className="font-semibold text-lg">Changes and corrections</p>
-            <div className="flex flex-col md:flex-row justify-between w-full gap-2 pt-5">
-                <div className=" relative flex justify-center items-center min-h-[15rem] min-w-[30rem] rounded-xl">
-                    <p className="text-white font-semibold text-2xl z-20">
-                        Change garage's installation{" "}
-                    </p>
-                    <div className="absolute top-0 left-0 h-full w-full z-10 bg-black opacity-30 hover:opacity-60 transition-opacity pointer rounded-xl"></div>
-                    <img
-                        src={change_info}
-                        alt=""
-                        className="absolute top-0 left-0 h-full w-full z-0 rounded-xl"
-                    />
+        <div className="w-full border-b-1 pt-5 pb-[7.5rem]">
+            <div className="flex justify-between items-center">
+                <p className="font-semibold text-3xl">Chỉnh sửa garage</p>
+                {/* <Tabs
+                    color="primary"
+                    aria-label="Tabs colors"
+                    radius="sm"
+                    selectedKey={selectType}
+                    onSelectionChange={(e: React.Key) => {
+                        console.log(e);
+                        setSelectType(e as string);
+                    }}
+                >
+                    <Tab key="today" title="Hôm nay" />
+                    <Tab key="tommorow" title="Ngày mai" />
+                </Tabs> */}
+            </div>
+            <div className="flex justify-between pt-10">
+                <div className="min-w-32 h-32 p-5 flex justify-center items-center shadow-md rounded-lg border-1 border-default-100">
+                    <div>
+                        <div className="font-semibold text-2xl flex gap-3 items-center">
+                            <FontAwesomeIcon icon={faCar} />
+                            <p>0</p>
+                        </div>
+                        <p className="text-lg pt-3">Check-ins</p>
+                    </div>
                 </div>
-
-                <div className=" relative flex justify-center items-center min-h-[15rem] min-w-[30rem] rounded-xl">
-                    <p className="text-white font-semibold text-2xl z-20">
-                        Change garage's installation{" "}
-                    </p>
-                    <div className="absolute top-0 left-0 h-full w-full z-10 bg-black opacity-30 hover:opacity-60 transition-opacity pointer rounded-xl"></div>
-                    <img
-                        src={change_info}
-                        alt=""
-                        className="absolute top-0 left-0 h-full w-full z-0 rounded-xl"
-                    />
+                <div className="min-w-32 h-32 p-5 flex justify-center items-center shadow-md rounded-lg border-1 border-default-100">
+                    <div>
+                        <div className="font-semibold text-2xl flex gap-3 items-center">
+                            <FontAwesomeIcon icon={faCar} />
+                            <p>0</p>
+                        </div>
+                        <p className="text-lg pt-3">Checkouts</p>
+                    </div>
                 </div>
-
-                <div className=" relative flex justify-center items-center min-h-[15rem] min-w-[30rem] rounded-xl">
-                    <p className="text-white font-semibold text-2xl z-20">
-                        Change garage's installation{" "}
-                    </p>
-                    <div className="absolute top-0 left-0 h-full w-full z-10 bg-black opacity-30 hover:opacity-60 transition-opacity pointer rounded-xl"></div>
-                    <img
-                        src={change_info}
-                        alt=""
-                        className="absolute top-0 left-0 h-full w-full z-0 rounded-xl"
-                    />
+                <div className="min-w-32 h-32 p-5 flex justify-center items-center shadow-md rounded-lg border-1 border-default-100">
+                    <div>
+                        <div className="font-semibold text-2xl flex gap-3 items-center">
+                            <FontAwesomeIcon icon={faCar} />
+                            <p>0</p>
+                        </div>
+                        <p className="text-lg pt-3">Đang sửa chữa</p>
+                    </div>
+                </div>
+                <div className="min-w-32 h-32 p-5 flex justify-center items-center shadow-md rounded-lg border-1 border-default-100">
+                    <div>
+                        <div className="font-semibold text-2xl flex gap-3 items-center">
+                            <FontAwesomeIcon icon={faCar} />
+                            <p>0</p>
+                        </div>
+                        <p className="text-lg pt-3">Chờ đánh giá</p>
+                    </div>
                 </div>
             </div>
         </div>
