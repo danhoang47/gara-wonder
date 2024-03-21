49f47cdf9a1a041dfa27ca3b2244cdbf7ac4ea08
naga105 <huy1152002@gmail.com>
2024-03-22 02:08:03 +0700

    pages/garage-management/general update link setting

--- apps/frontend/src/pages/garage-manage-page/general-dashboard/ui/update-garage/UpdateGarage.tsx
+++ apps/frontend/src/pages/garage-manage-page/general-dashboard/ui/update-garage/UpdateGarage.tsx
@@ -1,63 +1,37 @@
-import { faCar } from "@fortawesome/free-solid-svg-icons";
+import { faHandHoldingDollar, faSort } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Tab, Tabs } from "@nextui-org/react";
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function UpdateGarage() {
-    const [selectType, setSelectType] = useState<string>("today");
+    const navigate = useNavigate();
     return (
         <div className="w-full border-b-1 pt-5 pb-[7.5rem]">
             <div className="flex justify-between items-center">
                 <p className="font-semibold text-2xl">Chỉnh sửa garage</p>
-                {/* <Tabs
-                    color="primary"
-                    aria-label="Tabs colors"
-                    radius="sm"
-                    selectedKey={selectType}
-                    onSelectionChange={(e: React.Key) => {
-                        console.log(e);
-                        setSelectType(e as string);
-                    }}
-                >
-                    <Tab key="today" title="Hôm nay" />
-                    <Tab key="tommorow" title="Ngày mai" />
-                </Tabs> */}
             </div>
-            <div className="flex justify-between pt-10">
-                <div className="min-w-32 h-32 p-5 flex justify-center items-center shadow-md rounded-lg border-1 border-default-100">
+            <div className="flex gap-4 pt-10">
+                <div
+                    className="max-w-[15rem] h-32 p-5 flex justify-center items-center shadow-md hover:shadow-2xl rounded-lg border-1 border-default-100 cursor-pointer transition-shadow"
+                    onClick={() => navigate("./order-setting")}
+                >
                     <div>
                         <div className="font-semibold text-2xl flex gap-3 items-center">
-                            <FontAwesomeIcon icon={faCar} />
-                            <p>0</p>
+                            <FontAwesomeIcon icon={faSort} />
                         </div>
-                        <p className="text-lg pt-3">Check-ins</p>
+                        <p className="text-lg pt-3">Cài đặt chế độ nhận đơn</p>
                     </div>
                 </div>
-                <div className="min-w-32 h-32 p-5 flex justify-center items-center shadow-md rounded-lg border-1 border-default-100">
+                <div
+                    className="max-w-[15rem] h-32 p-5 flex justify-center items-center shadow-md hover:shadow-2xl rounded-lg border-1 border-default-100 cursor-pointer transition-shadow"
+                    onClick={() => navigate("./refund-setting")}
+                >
                     <div>
                         <div className="font-semibold text-2xl flex gap-3 items-center">
-                            <FontAwesomeIcon icon={faCar} />
-                            <p>0</p>
+                            <FontAwesomeIcon icon={faHandHoldingDollar} />
                         </div>
-                        <p className="text-lg pt-3">Checkouts</p>
-                    </div>
-                </div>
-                <div className="min-w-32 h-32 p-5 flex justify-center items-center shadow-md rounded-lg border-1 border-default-100">
-                    <div>
-                        <div className="font-semibold text-2xl flex gap-3 items-center">
-                            <FontAwesomeIcon icon={faCar} />
-                            <p>0</p>
-                        </div>
-                        <p className="text-lg pt-3">Đang sửa chữa</p>
-                    </div>
-                </div>
-                <div className="min-w-32 h-32 p-5 flex justify-center items-center shadow-md rounded-lg border-1 border-default-100">
-                    <div>
-                        <div className="font-semibold text-2xl flex gap-3 items-center">
-                            <FontAwesomeIcon icon={faCar} />
-                            <p>0</p>
-                        </div>
-                        <p className="text-lg pt-3">Chờ đánh giá</p>
+                        <p className="text-lg pt-3">
+                            Cài đặt Chế độ Hoàn trả thanh toán
+                        </p>
                     </div>
                 </div>
             </div>
