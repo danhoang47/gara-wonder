7eb039ba54e1b8ec21eb7a0ce65b963afb161178
naga105 <huy1152002@gmail.com>
2024-04-13 19:52:07 +0700

    pages/gara-detail fix Error

--- apps/frontend/src/api/garages/getScheduleSlot.ts
+++ apps/frontend/src/api/garages/getScheduleSlot.ts
@@ -3,15 +3,20 @@ import axios, { AxiosError } from "axios";
 import { Response } from "@/core/types";
 import { baseGaragesUrl } from ".";
 
+type queryType = {
+    startTime: number;
+    endTime: number;
+};
 export default async function getScheduleSlot(
     id: string | undefined,
-    month: number | string,
-    year: number | string,
+    queryParam: queryType,
 ) {
     try {
         let fetchUrl = baseGaragesUrl + `/${id}/management/schedule?`;
-        if (month) fetchUrl = fetchUrl + `month=${month}&`;
-        if (year) fetchUrl = fetchUrl + `year=${year}&`;
+        if (queryParam) {
+            fetchUrl = fetchUrl + `startTime=${queryParam.startTime}&`;
+            fetchUrl = fetchUrl + `endTime=${queryParam.endTime}`;
+        }
         const result = await axios.get<Response>(fetchUrl);
         return result.data.data;
     } catch (error) {
