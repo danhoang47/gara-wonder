6d21173309d8c7bbfe8fe383855446185eae5abb
Thinh Phan <phandthinhreal@gmail.com>
2024-04-26 08:51:42 +0700

    update API

--- apps/frontend/src/pages/account-page/order-detail/ui/user-info/UserInfo.tsx
+++ apps/frontend/src/pages/account-page/order-detail/ui/user-info/UserInfo.tsx
@@ -12,12 +12,12 @@ export default function UserInfo({ garageId }: { garageId?: string }) {
                 <p className="text-xl font-bold">Thông tin đơn hàng</p>
                 <div className="w-full h-1 border-t-2" />
                 <div className="flex  text-md">
-                    <div className="w-[15rem]">
+                    <div className="w-[20rem]">
                         <p className="text-default-400">Tên Garage</p>
                         <p>{garageData?.data[0].name}</p>
                     </div>
 
-                    <div className="w-[15rem]">
+                    <div className="w-[20rem]">
                         <p className="text-default-400">Địa chỉ</p>
                         <p> {garageData?.data[0].address}</p>
                     </div>
